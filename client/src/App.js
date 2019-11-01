39a79d713968992c2cb718181c3a18d6cca7974c
Curtis Shewchuk <curtis.shewchuk@queensu.ca>
2019-11-01 01:15:56 -0400

    appnavbar

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppNavBar from './components/AppNavBar/AppNavBar'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavBar/>
+    </div>
+  );
+}
+
+export default App;

cc0074735a796b898b0259fd3955d38e7cad5c29
Curtis Shewchuk <curtis.shewchuk@queensu.ca>
2019-11-04 15:10:20 -0500

    Completed Part 5 and 6

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import AppNavBar from './components/AppNavBar/AppNavBar';
 import ShoppingList from './components/ShoppingList/ShoppingList';
+import ItemModal from './components/ItemModal/ItemModal';
+import { Container } from 'reactstrap';
+
+import { Provider } from 'react-redux';
+import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <AppNavBar/>
-      <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar/>
+        <Container>
+          <ItemModal/>
+          <ShoppingList/>
+        </Container>
+      </div>
+    </Provider>
   );
 }
 

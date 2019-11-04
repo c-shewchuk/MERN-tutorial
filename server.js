af7303be458ed6ac7638533c849f19b612ee55e9
Curtis Shewchuk <curtis.shewchuk@queensu.ca>
2019-11-04 17:05:29 -0500

    make it better

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -18,6 +19,16 @@ const db = require('./config/keys').mongoURI;
 mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
+
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(expres.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
